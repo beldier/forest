33309b66201e30ad1a445744d5b3bb6f31247b1d
beldier <alvaro.yapu.cossio@gmail.com>
2023-08-06 22:16:04 -0400

    Day 1

--- src/main/kotlin/a/CF677.kt
+++ src/main/kotlin/a/CF677.kt
@@ -2,10 +2,27 @@ package a
 
 // https://codeforces.com/contest/677/problem/A
 // Vanya and fence
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val numbers= readInts()
+    var n = numbers[0]
+    val h = numbers[1]
+    var ans = 0
+    val heights = readInts()
+    for (i in 0 until n){
+        val curr = heights[i]
+        ans += if(curr> h)
+            2
+        else
+            1
+    }
+    println(ans)
 }
+
+private fun readLn() = readLine()!!
+private fun readInt() = readLn().toInt() // single int
+private fun readLong() = readLn().toLong() // single long
+private fun readDouble() = readLn().toDouble() // single double
+private fun readStrings() = readLn().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
+private fun readLongs() = readStrings().map { it.toLong() } // list of longs
+private fun readDoubles() = readStrings().map { it.toDouble() } // list of doubles
